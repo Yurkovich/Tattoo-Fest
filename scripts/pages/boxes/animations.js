1e23630f990393bcbf45163c04bd335605aa2e95
Yurkovich <yurkov200000@gmail.com>
2025-12-10 12:33:39 +0700

    Заменен плюс

--- scripts/pages/boxes/animations.js
+++ scripts/pages/boxes/animations.js
@@ -1,25 +1,18 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
-gsap.fromTo('.hero__title', {
-  opacity: 0,
-},
-{
+if (!window.innerWidth < 768) {
+  gsap.fromTo('.hero__title', { opacity: 0 },
+  {
   opacity: 1,
   duration: 1
-})
+  })
 
-gsap.fromTo('.hero__links', {
-  y: 100
-},
-{
-  y: 0
-})
+  gsap.fromTo('.hero__links', { y: 100 },
+  {
+    y: 0
+  })
 
-gsap.fromTo('.equipment', 
-  { 
-    opacity: 0, 
-    y: 100,
-  }, 
+  gsap.fromTo('.equipment', { opacity: 0, y: 100 }, 
   { 
     opacity: 1, 
     y: 0,
@@ -32,3 +25,5 @@ gsap.fromTo('.equipment',
     duration: 1
   }
 );
+}
+
